2fe64ccad0643c47ed2fa9004e1d19100fabc7f9
Santiago Rojas <sajoraho@gmail.com>
2021-12-01 20:07:34 -0500

    Data Sources creado

---
+++ src/dataSources/clinica_api.js
@@ -0,0 +1,26 @@
+const { RESTDataSource } = require('apollo-datasource-rest')
+const serverConfig = require('../server')
+class ClinicaAPI extends RESTDataSource {
+    constructor() {
+        super()
+        this.baseURL = serverConfig.clinica_api_url
+    }
+
+    async createPatient(patient) {
+        patient = new Object(JSON.parse(JSON.stringify(patient)))
+        return await this.post('/patient', patient)
+    }
+
+    async patienById(id) {
+        return await this.get(`/patient/${id}`)
+    }
+
+    async updatePatient(patient) {
+        patient = new Object(JSON.parse(JSON.stringify(patient)))
+        return await this.put(`/patient/${id}`, patient)
+    }
+
+    async deletePatient(patient) {
+        return await this.delete(`/patient/${id}`, patient)
+    }
+}
